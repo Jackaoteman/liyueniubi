f877430ec0eeeaa9865961ebe0b7146dec2dc2b5
Jack <Jack@163.com>
2020-06-07 23:31:44 +0800

    修改屏障

--- src/test/java/com/liyue/liyueniubi/LiyueniubiApplicationTests.java
+++ src/test/java/com/liyue/liyueniubi/LiyueniubiApplicationTests.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 @SpringBootTest
 class LiyueniubiApplicationTests {
@@ -19,10 +21,13 @@ class LiyueniubiApplicationTests {
     private  static  int success_num=0;
     //技术器 用户统计卖出商品数量
     private  static  int  sell_num=0;
-
+    //屏障器，统计所有线程完成时间
+    private CyclicBarrier cyclicBarrier=new CyclicBarrier(user_num+1);
 
     @Test
     void contextLoads() throws InterruptedException {
+
+        long startTime = System.currentTimeMillis();
 
         for (int i = 0; i < user_num; i++) {
             new Thread(new userRequest()).start();
@@ -31,6 +36,14 @@ class LiyueniubiApplicationTests {
             }
             countDownLatch.countDown();
         }
+        try {
+            //裁判先到达 一共1001个屏障器
+            cyclicBarrier.await();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        System.out.println("一共执行="+(System.currentTimeMillis()-startTime/1000)+"秒");
+
     }
     public  class userRequest implements  Runnable{
 
@@ -39,19 +52,27 @@ class LiyueniubiApplicationTests {
 
         @Override
         public void run() {
-
             try {
-                //进入等待状态
+                //进入等待状态 所有请求全部进入起点开始执行
                 countDownLatch.await();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
+            //逻辑修改
+            //
+            //
+            //
+            //
 
-
-
-
-
+            //每个线程请求完到达 await
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
         }
     }
 
